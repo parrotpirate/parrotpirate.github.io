e00120ca4a672fb9111df088fdd015f3e6de0c19
Stephen Parker <parrotpirate@gmail.com>
2015-12-02 13:38:09 -0500

    Responsive nav
    Rebuilt nav menu to collapse and hide on mobile devices.  Replaced with
    small button that toggles list of nav items.

--- script/index.js
+++ script/index.js
@@ -6,4 +6,26 @@ jQuery(document).ready(function($) {
 		$('html, body').animate({
 			scrollTop: $('.portfolio').offset().top}, '0.3s');
 	});
+
+	// *Menu display toggle for mobile format*
+	$('.nav-display-button button').click(function(e) {
+		/* Act on the event */
+		e.preventDefault();
+		$('.navbar').slideToggle('fast');
+	});
+
+	$(window).resize(function(event) {
+		/* Act on the event */
+		if ($(window).width() > 800) {
+			$('.navbar').css('display', 'inline-block');
+		}
+	});
+
+	$(window).resize(function(event) {
+		/* Act on the event */
+		if ($(window).width() <= 800) {
+			$('.navbar').css('display', 'none');
+		}
+	});
+	
 });
